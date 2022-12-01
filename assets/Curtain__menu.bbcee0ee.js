38d5012460f1190b6e9b8013afaf3f6b177a9553
unknown <m.gouillardon@codeur.online>
2022-12-01 13:35:52 +0100

    feat: project v1

---
+++ assets/Curtain__menu.bbcee0ee.js
@@ -0,0 +1,10 @@
+const a=`
+<div class="menu__overlay" id="menu__overlay-js">
+    <ul class="menu__list" id="menu__list-js">
+        <li class="menu__item"><a href="#">HOME</a></li>
+        <li class="menu__item"><a href="#work">WORK</a></li>
+        <li class="menu__item"><a href="#about">ABOUT</a></li>
+        <li class="menu__item"><a href="#contact">CONTACT</a></li>
+    </ul>
+</div>
+`;export{a as default};
---
+++ assets/Navbar.499d3333.js
@@ -0,0 +1,15 @@
+const a=`
+<nav class="navbar">
+      <div class="navbar__name" id="navbar__name-js">Portfolio 2022</div>
+      <div class="navbar__line"></div>
+      <ul class="navbar__list">
+        <li class="navbar__item"><a href="#">Home</a></li>
+        <li class="navbar__item"><a href="#work">Work</a></li>
+        <li class="navbar__item"><a href="#about">About</a></li>
+        <li class="navbar__item"><a href="#contact">Contact</a></li>
+      </ul>
+      <div class="navbar__burger-btn" id="burger-btn">
+        <span></span>
+      </div>
+</nav>
+`;export{a as default};
