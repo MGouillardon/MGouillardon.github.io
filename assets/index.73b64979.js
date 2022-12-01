933c87f85ca1a83df72194fb50235ae8a9225712
unknown <m.gouillardon@codeur.online>
2022-12-01 15:28:30 +0100

    fix: path img

--- assets/index.73b64979.js
+++ assets/index.73b64979.js
@@ -66,7 +66,7 @@ object-assign
           <div class="work__kluik" data-scroll-section>
             <div class="work__kluik__img__container">
               <div class="work__kluik__img">
-              <img data-scroll data-scroll-speed="6" src="/kluik.png" alt="kluik project picture">
+              <img data-scroll data-scroll-speed="6" src="./kluik.png" alt="kluik project picture">
               </div>
             </div>
             <div class="work__kluik__desc">
@@ -86,14 +86,14 @@ object-assign
             </div>
             <div class="work__TDEE__img__container">
               <div class="work__TDEE__img">
-              <img data-scroll data-scroll-speed="6" src="/TDEE.png" alt="TDEE project picture">
+              <img data-scroll data-scroll-speed="6" src="./TDEE.png" alt="TDEE project picture">
               </div>
             </div>
           </div>
           <div class="work__quizzos" data-scroll-section>
             <div class="work__quizzos__img__container">
               <div class="work__quizzos__img">
-              <img data-scroll data-scroll-speed="6" src="/quizzos.png" alt="quizzos project picture">
+              <img data-scroll data-scroll-speed="6" src="./quizzos.png" alt="quizzos project picture">
               </div>
             </div>
             <div class="work__quizzos__desc">
@@ -113,7 +113,7 @@ object-assign
             </div>
             <div class="work__awesome__img__container">
               <div class="work__awesome__img">
-              <img data-scroll data-scroll-speed="6" src="/e-commerce.png" alt="awesome project picture">
+              <img data-scroll data-scroll-speed="6" src="./e-commerce.png" alt="awesome project picture">
               </div>
             </div>
           </div>
